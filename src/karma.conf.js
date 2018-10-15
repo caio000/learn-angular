a07a1d770fc1a32a5ce4918b77b03a1a3518eb04
Caio de Freitas <caiof.adriano@hotmail.com>
2018-10-15 10:40:25 -0300

    Atualizado configurações do travis CI e Karma.js

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -3,6 +3,13 @@
 
 module.exports = function (config) {
   config.set({
+    browsers: ['MyHeadlessChrome'],
+    customLauchers: {
+      MyHeadlessChrome: {
+        base: 'ChromeHeadless',
+        flags: ['--disable-translate', '--disable-extensions', 'remote-debugging-port=9223', '--no-sandbox']
+      }
+    },
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
@@ -25,7 +32,6 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
-    browsers: ['Chrome'],
     singleRun: true
   });
 };
