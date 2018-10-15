19e2bd277dac5c46c5cd5713b77057334abd4c6f
Caio de Freitas <caiof.adriano@hotmail.com>
2018-10-15 11:12:46 -0300

    Update karma config

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -3,31 +3,36 @@
 
 module.exports = function (config) {
   config.set({
-    browsers: ['MyHeadlessChrome'],
     customLauchers: {
-      MyHeadlessChrome: {
-        base: 'ChromeHeadless',
-        flags: ['--disable-translate', '--disable-extensions', 'remote-debugging-port=9223', '--no-sandbox']
+      ChromeHeadless: {
+        base: "Chrome",
+        flags: [
+          "--disable-translate",
+          "--disable-extensions",
+          "remote-debugging-port=9223",
+          "--no-sandbox"
+        ]
       }
     },
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    browsers: ["ChromeHeadless"],
+    basePath: "",
+    frameworks: ["jasmine", "@angular-devkit/build-angular"],
     plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require("karma-jasmine"),
+      require("karma-chrome-launcher"),
+      require("karma-jasmine-html-reporter"),
+      require("karma-coverage-istanbul-reporter"),
+      require("@angular-devkit/build-angular/plugins/karma")
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage'),
-      reports: ['html', 'lcovonly'],
+      dir: require("path").join(__dirname, "../coverage"),
+      reports: ["html", "lcovonly"],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ["progress", "kjhtml"],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
