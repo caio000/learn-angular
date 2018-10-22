ede27026b95b4d6944539e67c5822a2613511efa
Caio de Freitas <caiof.adriano@hotmail.com>
2018-10-22 13:43:11 -0300

    Diretiva de estrutura ngElse

--- src/app/diretivas-customizadas/diretivas-customizadas.component.ts
+++ src/app/diretivas-customizadas/diretivas-customizadas.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, TemplateRef, ViewContainerRef } from '@angular/core';
 
 @Component({
   selector: 'app-diretivas-customizadas',
