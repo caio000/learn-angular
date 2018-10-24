fab62196bb608ed4be6b258f7c428ae18e7ffa71
Caio de Freitas <caiof.adriano@hotmail.com>
2018-10-24 10:57:17 -0300

    Criado testes para o serviço de curso

--- src/app/cursos/cursos.service.spec.ts
+++ src/app/cursos/cursos.service.spec.ts
@@ -12,4 +12,9 @@ describe('CursosService', () => {
   it('should be created', inject([CursosService], (service: CursosService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('should be created a new course', inject([CursosService], (service: CursosService) => {
+      service.add('nodejs');
+      expect(service.getCursos()).toContain('nodejs');
+  }));
 });
