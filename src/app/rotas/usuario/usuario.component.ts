baf4afcc317380a02b4cdcd1ad1867f65b3477de
Caio de Freitas <caiof.adriano@hotmail.com>
2018-11-27 17:30:29 -0200

    Criando query params

--- src/app/rotas/usuario/usuario.component.ts
+++ src/app/rotas/usuario/usuario.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { Subscription } from 'rxjs';
 
 import { UsuarioService } from './usuario.service';
 import { Usuario } from './usuario';
@@ -9,20 +11,26 @@ import { Usuario } from './usuario';
   templateUrl: './usuario.component.html',
   styleUrls: ['./usuario.component.css']
 })
-export class UsuarioComponent implements OnInit {
+export class UsuarioComponent implements OnInit, OnDestroy {
 
     public id;
-
+    public pagina: number;
     public usuarios: Array<Usuario>;
+    private querySubscription: Subscription;
 
   constructor(
       private usuarioService: UsuarioService,
-      private route: ActivatedRoute
+      private route: ActivatedRoute,
+      private router: Router,
   ) { }
 
   ngOnInit() {
       this.route.params.subscribe((params: any) => {
           this.id = params['id'];
+      });
+
+      this.querySubscription = this.route.queryParams.subscribe((params: any) => {
+          this.pagina = params.pagina;
       });
       console.log(this.getRouteId());
       console.log(this.id);
@@ -30,8 +38,24 @@ export class UsuarioComponent implements OnInit {
       this.usuarios = this.usuarioService.getAll();
   }
 
+  ngOnDestroy() {
+      this.querySubscription.unsubscribe();
+  }
+
   getRouteId() {
       return this.route.snapshot.params['id'];
+  }
+
+  nextPage(): void {
+      this.router.navigate(['usuarios'], {
+          queryParams: { 'pagina': ++this.pagina },
+      });
+  }
+
+  previousPage(): void {
+      this.router.navigate(['/usuarios'], {
+          queryParams: { 'pagina': --this.pagina },
+      });
   }
 
 }
