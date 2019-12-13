1d4bffbaa20ec6ea1006c3a72bff9982f3ed563c
Caio de Freitas <caiof.adriano@hotmail.com>
2019-12-13 16:31:41 -0300

    Fix

--- src/app/shared/fundo-amarelo.directive.ts
+++ src/app/shared/fundo-amarelo.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Renderer } from '@angular/core';
+import { Directive, ElementRef, Renderer, Renderer2 } from '@angular/core';
 
 @Directive({
     selector: 'p[appFundoAmarelo]' // essa diretiva só pode ser utilizadas por tags P
@@ -6,9 +6,10 @@ import { Directive, ElementRef, Renderer } from '@angular/core';
 export class FundoAmareloDirective {
     constructor(
         private _elementRef: ElementRef,
-        private _renderer: Renderer,
+        private _renderer: Renderer2,
     ) {
         // this._elementRef.nativeElement.style.backgroundColor = 'yellow';
-        this._renderer.setElementStyle(this._elementRef.nativeElement, 'background-color', 'yellow');
+        this._renderer.setStyle(this._elementRef.nativeElement, 'background-color', 'yellow');
+        // this._renderer.setElementStyle(this._elementRef.nativeElement, 'background-color', 'yellow');
     }
 }
